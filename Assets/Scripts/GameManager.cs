e5f2360a439a282705e5ed56c49044bf55e85cae
Sam Marchenko <sam.marchenko@gmail.com>
2023-02-14 16:43:31 +0300

    Полностью убрал цикл for в GameManager при апдейте трассы

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,20 +10,17 @@ namespace Runner
         private float _step = 6f;
         private int _currentIndex = 0;
         private float _lastZ = 30f;
-        
+
 
         [SerializeField, Range(1, 100), Tooltip("Это здоровье игрока, не перепутай")]
         private int _health = 10;
-        [SerializeField]
-        private Transform _player;
-        [SerializeField]
-        private Transform[] _levels;
-        [SerializeField, Space]
-        private Text _progressText;
+
+        [SerializeField] private Transform _player;
+        [SerializeField] private Transform[] _levels;
+        [SerializeField, Space] private Text _progressText;
 
 
         public static GameManager Self { get; private set; }
-
 
 
         void Awake()
@@ -31,23 +28,23 @@ namespace Runner
             Self = this;
         }
 
-		private void Update()
-		{
+        private void Update()
+        {
             if (_player.position.y <= -1f) SetDamage();
         }
 
-		public void SetDamage()
+        public void SetDamage()
         {
             _health -= 1;
 
             Debug.Log("Current health: " + _health);
 
-            if(_health <= 0)
+            if (_health <= 0)
             {
                 Debug.Log("---Игрок погиб!---");
                 UnityEditor.EditorApplication.isPaused = true;
-			}
-		}
+            }
+        }
 
         public void UpdateLevel()
         {
@@ -55,18 +52,16 @@ namespace Runner
             _progressText.text = _progress.ToString();
 
             _lastZ += _step;
-            for(int i = 0; i < _levels.Length; i++)
-            {
-                var position = _levels[_currentIndex].position;
-                position.z = _lastZ;
-                _levels[_currentIndex].position = position;
-            }
 
+            var position = _levels[_currentIndex].position;
+            position.z = _lastZ;
+            _levels[_currentIndex].position = position;
+            
             _currentIndex++;
             if (_currentIndex >= _levels.Length)
             {
                 _currentIndex = 0;
             }
-	    }
+        }
     }
 }
--- Assets/Scripts/TriggerComponent.cs
+++ Assets/Scripts/TriggerComponent.cs
@@ -4,24 +4,17 @@ namespace Runner
 {
     public class TriggerComponent : MonoBehaviour
     {
-        [SerializeField]
-        private Collider _collider;
+        [SerializeField] private Collider _collider;
 
-		[SerializeField]
-		private bool _isDamage;
+        [SerializeField] private bool _isDamage;
 
         void Start()
         {
             _collider.isTrigger = true;
         }
 
-		private void OnTriggerEnter(Collider other)
-		{
-            Debug.Log($"Я вызываюсь на объекте {gameObject.name} из-за реакции на {other.gameObject.name}");
-            // if (!other.CompareTag("Player"))
-            // {
-            //     return;
-            // }
+        private void OnTriggerEnter(Collider other)
+        {
             if (_isDamage)
             {
                 GameManager.Self.SetDamage();
@@ -29,7 +22,7 @@ namespace Runner
             else
             {
                 GameManager.Self.UpdateLevel();
-			}
+            }
         }
-	}
+    }
 }
